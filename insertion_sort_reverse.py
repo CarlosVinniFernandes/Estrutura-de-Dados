c8cadd9ca5ffc9fd65336ea3ef49837cb4173dd3
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2025-01-16 20:32:22 -0300

    Create insertion_sort_reverse.py

---
+++ insertion_sort_reverse.py
@@ -0,0 +1,18 @@
+lista = [5, 2, 4, 6, 1, 10, 3]
+
+def insertion_sort(lista):
+    for i in range(1, len(lista)):
+        atual = lista[i]
+        
+        j = i - 1
+        
+        while j >= 0 and lista[j] < atual:
+            lista[j + 1] = lista[j]
+            
+            j -= 1
+            
+            lista[j + 1] = atual
+    return lista
+
+insertion_sort(lista)
+print(lista)
